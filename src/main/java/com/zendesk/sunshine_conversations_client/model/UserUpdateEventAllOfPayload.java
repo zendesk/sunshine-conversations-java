70a7d012740aed62c62343c6246edb4f681c3f99
zendesk-ops-ci <zendesk-ops-ci@zendesk.com>
2022-06-23 15:56:03 +0000

    Generate version 9.7.2

--- src/main/java/com/zendesk/sunshine_conversations_client/model/UserUpdateEventAllOfPayload.java
+++ src/main/java/com/zendesk/sunshine_conversations_client/model/UserUpdateEventAllOfPayload.java
@@ -39,7 +39,7 @@ public class UserUpdateEventAllOfPayload {
   private User user = null;
 
   /**
-   * The reason why the user was updated, if applicable. * &#x60;authentication&#x60; - An anonymous user became an [identified](https://docs.smooch.io/guide/intro-to-users/) user. * &#x60;localeDetection&#x60; - A user&#39;s was updated as a result of automated locale detection on messages sent. 
+   * The reason why the user was updated, if applicable. * &#x60;authentication&#x60; - An anonymous user became an [identified](https://docs.smooch.io/guide/intro-to-users/) user. * &#x60;localeDetection&#x60; - A user was updated as a result of automated locale detection on messages sent. 
    */
   public enum ReasonEnum {
     AUTHENTICATION("authentication"),
@@ -112,11 +112,11 @@ public class UserUpdateEventAllOfPayload {
   }
 
    /**
-   * The reason why the user was updated, if applicable. * &#x60;authentication&#x60; - An anonymous user became an [identified](https://docs.smooch.io/guide/intro-to-users/) user. * &#x60;localeDetection&#x60; - A user&#39;s was updated as a result of automated locale detection on messages sent. 
+   * The reason why the user was updated, if applicable. * &#x60;authentication&#x60; - An anonymous user became an [identified](https://docs.smooch.io/guide/intro-to-users/) user. * &#x60;localeDetection&#x60; - A user was updated as a result of automated locale detection on messages sent. 
    * @return reason
   **/
   @javax.annotation.Nullable
-  @ApiModelProperty(value = "The reason why the user was updated, if applicable. * `authentication` - An anonymous user became an [identified](https://docs.smooch.io/guide/intro-to-users/) user. * `localeDetection` - A user's was updated as a result of automated locale detection on messages sent. ")
+  @ApiModelProperty(value = "The reason why the user was updated, if applicable. * `authentication` - An anonymous user became an [identified](https://docs.smooch.io/guide/intro-to-users/) user. * `localeDetection` - A user was updated as a result of automated locale detection on messages sent. ")
   @JsonProperty(JSON_PROPERTY_REASON)
   @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
 
